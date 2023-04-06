06135f6ae89b73b346eab57d59e04d6e6b9bb912
pradeep <pradeepsn606@gmail.com>
2023-04-06 20:02:11 +0530

    extended student models

--- student/models.py
+++ student/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator
   
 class Student(models.Model):
-    regno = models.IntegerField(blank=False,null=False,primary_key=True,db_index=True)
+    regno = models.CharField(blank=False,null=False,primary_key=True,db_index=True,unique=True,max_length=10)
     name = models.CharField(max_length=200)
     course = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
@@ -11,8 +11,8 @@ class Student(models.Model):
     contact_no = models.CharField(max_length=30)
     address = models.CharField(max_length=600)
     is_hostelite = models.BooleanField()
-    family_details = models.OneToOneField('student.FamilyDets',on_delete=models.CASCADE,related_name='student')
-    past_academics = models.OneToOneField('student.PastAcademics',on_delete=models.CASCADE,related_name='student')
+    family_details = models.OneToOneField('student.FamilyDets',on_delete=models.CASCADE,related_name='Student')
+    past_academics = models.OneToOneField('student.PastAcademics',on_delete=models.CASCADE,related_name='Student')
     # attendance = models.OneToOneField('student.Attendance',on_delete=models.CASCADE,related_name='student')
 
 class FamilyDets(models.Model):
@@ -29,16 +29,16 @@ class PastAcademics(models.Model):
     cut_off = models.DecimalField(max_digits=15,decimal_places=6,blank = True,null=True)
     cgpa = models.DecimalField(max_digits  = 15,decimal_places=6,blank = True , null=True)
     percentage = models.DecimalField(max_digits= 15, decimal_places=6, blank = True, null=True)
-    other_exams = models.OneToOneField('student.PastOtherExams',on_delete=models.CASCADE)
 
 class PastOtherExams(models.Model):
+    roll_no= models.ForeignKey('student.Student',on_delete=models.CASCADE,related_name='PastOtherExams')
     year_of_exam = models.DateField()
     exam_name = models.CharField(max_length=100)
     result = models.DecimalField(max_digits=15,decimal_places=6)
     total = models.DecimalField(max_digits=15,decimal_places=6)
 
 class Attendance(models.Model):
-    student = models.ForeignKey('student.Student',on_delete=models.CASCADE,related_name='Attendance')
+    roll_no = models.ForeignKey('student.Student',on_delete=models.CASCADE,related_name='Attendance')
     semester = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(12)])
     # entry_no = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
     total_working_days = models.IntegerField()
@@ -57,13 +57,83 @@ class AbsentDetails(models.Model):
         ("OD" , "OD"),
         ("Late" , " Late")
     ]
-    attendance = models.OneToOneField('student.Attendance',on_delete=models.CASCADE,related_name='attendance')
+    roll_no = models.ForeignKey('student.Student',on_delete=models.CASCADE,related_name='AbsentDetails')
     semester = models.IntegerField(validators=[MaxValueValidator(10),MinValueValidator(1)])
     date_from = models.DateField()
     date_to = models.DateField()
     absent_type = models.CharField(max_length=20,choices=absent_type_choices)
     desc = models.CharField(max_length=200)
     
+# ####
+class InternalPerformance(models.Model):
+    roll_no=models.ForeignKey('student.student',on_delete=models.CASCADE,related_name='AcademicDetails')
+    semester_no=models.IntegerField(validators=[MaxValueValidator(12),MinValueValidator(1)])
+    exam_type = models.CharField(max_length=100)
+    eaxm_number = models.CharField(max_length=100)
+    scored_marks = models.DecimalField(max_digits=5,decimal_places=2)
+    total_marks = models.DecimalField(max_digits=5,decimal_places=2)
+    percentage_scored = models.DecimalField(max_digits=5,decimal_places=2)
+    
+    def save(self,*args,**kwargs):
+        super.save(*args,**kwargs)
+        self.percentage_scored = (self.scored_marks/self.total_marks) * 100
+
+class SemesterPerformance(models.Model):
+    grade_choices=[
+        ('O','O GRADE'),
+        ('A+','A+ GRADE'),
+        ('A','A GRADE'),
+        ('B+','B+ GRADE'),
+        ('B','B GRADE'),
+        ('C','C GRADE'),
+        ('U','FAIL'),
+        ('AB','ABSENT'),
+        ('MM','MALPRACTICE')
+    ]
+    roll_no = models.OneToOneField('student.AcademicDetails',on_delete=models.CASCADE,related_name='SemesterPerformance')
+    semester_no=models.IntegerField(validators=[MaxValueValidator(12),MinValueValidator(1)])
+    subject_code=models.CharField(max_length=10)
+    grade=models.CharField(max_length=2,choices=grade_choices)
+    cgpa = models.DecimalField(max_digits=5,decimal_places=3)
+
+
+class Projects(models.Model):
+    roll_no = models.OneToOneField('student.Student',on_delete=models.CASCADE,related_name='Projects')
+    start_Date=models.DateField()
+    end_Date=models.DateField()
+    organisation=models.CharField(max_length=100)
+    specialization=models.CharField(max_length=200)
+    certificate=models.FileField()
+
+
+class Achievements(models.Model):
+    status_choices=(
+        ('win-1','winner'),
+        ('win-2','runner'),
+        ('win-3','second-runner'),
+        ('participated','partcipated'),
+    )
+    roll_no = models.ForeignKey('student.Student',on_delete=models.CASCADE,related_name='Achievements')
+    date=models.DateField()
+    organisation=models.CharField(max_length=300)
+    event_name=models.CharField(max_length=200)
+    document=models.FileField(upload_to = '/achievements',blank=True,null=True)
+    status=models.CharField(max_length=10,choices=status_choices)
+
+
+class PlacementDetails(models.Model):
+    roll_no=models.ForeignKey('student.Student',on_delete=models.CASCADE,related_name='PlacementDetails')
+    company_name=models.CharField(max_length=300)
+    date=models.DateField()
+    results=models.BooleanField()
+    package=models.DecimalField()
+
+
+class DisciplinaryDetails(models.Model):
+    roll_no=models.ForeignKey('student.Student',on_delete=models.CASCADE,related_name='DisiplinaryDetails')
+    date=models.DateField()
+    description=models.CharField(max_length=100)
+    document = models.FileField(upload_to='/disiplinary',blank=True,null=True)
 
 
 
--- student/serializers.py
+++ student/serializers.py
@@ -1,13 +1,10 @@
 from rest_framework import serializers
-from . models import Attendance,AbsentDetails,FamilyDets,PastAcademics,PastOtherExams,Student
+from . models import Attendance,AbsentDetails,FamilyDets
+from . models import PastAcademics,PastOtherExams,Student
+from .models import InternalPerformance,SemesterPerformance,Projects,Achievements
+from .models import PlacementDetails,DisciplinaryDetails
 # partial = True for serializer.save()
 
-# insert absent Details without attendance entry
-class AbsentDetailsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AbsentDetails
-        fields = '__all__'
-        exclude = ['id']
 
 #insert family details only with student init data
 class FamilyDetsSerializer(serializers.ModelSerializer):
@@ -15,33 +12,15 @@ class FamilyDetsSerializer(serializers.ModelSerializer):
         model = FamilyDets
         fields = '__all__'
         exclude = ['id']
+
 #insert Past academic details only with student init data
 class PastAcademicsSerializer(serializers.ModelSerializer):
     class Meta:
         model = PastAcademics
         fields = '__all__'
         exclude = ['id']
-#insert Past other exams only with student init data
-class PastOtherExams(serializers.ModelSerializer):
-    class Meta:
-        model = PastOtherExams
-        fields = '__all__'
-        exclude = ['id']
+        
 
-# to view and insert attendance and absent reason together
-class AttendanceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Attendance
-        fields = '__all__'
-        exclude = ['id']
-
-    # Insert attendance data along with absent reason
-    absent_dets = AbsentDetailsSerializer(many=False)
-    def create(self, validated_data):
-        absent_details = validated_data.pop('absent_details')
-        student_attendance = Attendance.objects.create(**validated_data)
-        AbsentDetails.objects.create(attendance = student_attendance , **absent_details)
-        return student_attendance
 
 # Enter student init data along with family_details and past_academics
 class StudentSerializer(serializers.ModelSerializer):
@@ -57,9 +36,68 @@ class StudentSerializer(serializers.ModelSerializer):
         family_dets = validated_data.pop('family_dets')
         past_academics = validated_data.pop('past_academics')
         past_other_exams = past_academics.pop('past_other_exams')
-        # past_other_exams = validated
         family_instance = FamilyDets.objects.create(**family_dets)
-        past_other_exams_instance = PastOtherExams.objects.create(**past_other_exams)
-        past_academics_instance = PastAcademics.objects.create(other_exams = past_other_exams_instance,**past_academics)
-        student_instance = Student.objects.create(family_details = family_instance,past_academics = past_academics_instance, **validated_data)
+        past_academics_instance = PastAcademics.objects.create(**past_academics)
+        student_instance = Student.objects.create(family_details = family_instance,
+                                                  past_academics = past_academics_instance,
+                                                    **validated_data)
+        PastOtherExams.objects.create(roll_no = student_instance,**past_other_exams)
         return student_instance
+
+
+# insert absent Details without attendance entry
+class AbsentDetailsSerializer(serializers.ModelSerializer):
+    student_instance = StudentSerializer()
+    class Meta:
+        model = AbsentDetails
+        fields = '__all__'
+        exclude = ['id']
+
+#insert Past other exams only with student init data
+class PastOtherExamsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PastOtherExams
+        fields = '__all__'
+        exclude = ['id']
+
+# to view and insert attendance 
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = '__all__'
+        exclude = ['id']
+
+class InternalPerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InternalPerformance
+        fields = "__all__"
+        exclude = ['id']
+    
+class SemesterPerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SemesterPerformance
+        fields = "__all__"
+        exclude = ['id']
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = "__all__"
+        exclude = ['id']
+
+class AchievementsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Achievements
+        fields = "__all__"
+        exclude = ['id']
+class PlacementDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlacementDetails
+        fields = "__all__"
+        exclude = ['id']
+
+class DisciplinaryDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DisciplinaryDetails
+        fields = "__all__"
+        exclude = ['id']
